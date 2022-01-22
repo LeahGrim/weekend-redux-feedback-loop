3d32ea8ea314214094bd4f4b949ff8a68b471ba4
Subash Khatri <subashkhatri44@gmail.com>
2022-01-22 15:15:15 -0600

    worked on Feelings

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+import Feelings from '../Feelings/Feelings'
+import{ HashRouter as Router, Route, Link} from 'react-router-dom';
+
+
 function App() {
 
   return (
+
     <div className='App'>
       <header className='App-header'>
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+      <Router>
+        <Link to="/Feelings">Start</Link>
+        <Route exact path="/Feelings">
+      <Feelings />
+      </Route>
+      </Router>
     </div>
+
   );
 }
 
